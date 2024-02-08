90f7719d3fabf79f21a41885e1a0e9c97098934d
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2024-02-08 23:26:11 +0300

    refactor: remake main page & styles

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,14 @@
-console.log("start Bilim project");
+const HEADER_BUTTON = document.querySelector(".header-menu");
+const HEADER_NAV = document.querySelector(".header-nav");
+
+HEADER_BUTTON.addEventListener("click", toggleHeaderButton);
+
+function toggleHeaderButton() {
+  if (HEADER_BUTTON.classList.contains("menu-active")) {
+    HEADER_BUTTON.classList.remove("menu-active");
+    HEADER_NAV.classList.remove("header-nav-active");
+  } else {
+    HEADER_BUTTON.classList.add("menu-active");
+    HEADER_NAV.classList.add("header-nav-active");
+  }
+}
