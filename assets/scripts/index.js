db7e0448b07d4e5a9170ca6a42fd50cb08749f4d
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2024-02-09 13:12:31 +0300

    feat: make header menu to main & about page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,5 +1,6 @@
 const HEADER_BUTTON = document.querySelector(".header-menu");
 const HEADER_NAV = document.querySelector(".header-nav");
+const HEADER_LOGO = HEADER_NAV.querySelector(".header-logo");
 
 HEADER_BUTTON.addEventListener("click", toggleHeaderButton);
 
@@ -7,8 +8,10 @@ function toggleHeaderButton() {
   if (HEADER_BUTTON.classList.contains("menu-active")) {
     HEADER_BUTTON.classList.remove("menu-active");
     HEADER_NAV.classList.remove("header-nav-active");
+    HEADER_LOGO.style.opacity = "1";
   } else {
     HEADER_BUTTON.classList.add("menu-active");
     HEADER_NAV.classList.add("header-nav-active");
+    HEADER_LOGO.style.opacity = "0";
   }
 }
