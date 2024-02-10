e24d5c44e5f20c68c720d2e7971cba505e3c3eeb
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2024-02-10 16:37:21 +0300

    refactor: change style logo in header

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,17 +1,12 @@
+const HEADER = document.querySelector(".header");
 const HEADER_BUTTON = document.querySelector(".header-menu");
-const HEADER_NAV = document.querySelector(".header-nav");
-const HEADER_LOGO = HEADER_NAV.querySelector(".header-logo");
 
 HEADER_BUTTON.addEventListener("click", toggleHeaderButton);
 
 function toggleHeaderButton() {
-  if (HEADER_BUTTON.classList.contains("menu-active")) {
-    HEADER_BUTTON.classList.remove("menu-active");
-    HEADER_NAV.classList.remove("header-nav-active");
-    HEADER_LOGO.style.opacity = "1";
+  if (HEADER.classList.contains("header-active")) {
+    HEADER.classList.remove("header-active");
   } else {
-    HEADER_BUTTON.classList.add("menu-active");
-    HEADER_NAV.classList.add("header-nav-active");
-    HEADER_LOGO.style.opacity = "0";
+    HEADER.classList.add("header-active");
   }
 }
