f26b2c28200347e121021cc93d810657d4bbe446
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2024-02-11 19:07:54 +0300

    feat: correct logo in services page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,19 @@
 const HEADER = document.querySelector(".header");
 const HEADER_BUTTON = document.querySelector(".header-menu");
+const LOGO = document.querySelector(".logo");
 
 HEADER_BUTTON.addEventListener("click", toggleHeaderButton);
 
 function toggleHeaderButton() {
   if (HEADER.classList.contains("header-active")) {
     HEADER.classList.remove("header-active");
+    if (LOGO !== null && LOGO.classList.contains("unactive")) {
+      LOGO.classList.remove("unactive");
+    }
   } else {
     HEADER.classList.add("header-active");
+    if (LOGO !== null) {
+      LOGO.classList.add("unactive");
+    }
   }
 }
