2fd51bca58b14471db7aab3883e031917953b256
Aleks_bass <105873742+izmailowAlex@users.noreply.github.com>
2024-02-11 20:11:16 +0300

    refactor: change block sizes on services page

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,10 +1,17 @@
 const HEADER = document.querySelector(".header");
 const HEADER_BUTTON = document.querySelector(".header-menu");
 const LOGO = document.querySelector(".logo");
+const SERVICES_LIST = document.querySelectorAll(".services-item");
 
-HEADER_BUTTON.addEventListener("click", toggleHeaderButton);
+HEADER_BUTTON.addEventListener("click", handleToggleHeaderButton);
+SERVICES_LIST.forEach((item) => {
+  item.addEventListener("click", () => {
+    cleanActiveInItem();
+    addActiveInItem(item);
+  });
+});
 
-function toggleHeaderButton() {
+function handleToggleHeaderButton() {
   if (HEADER.classList.contains("header-active")) {
     HEADER.classList.remove("header-active");
     if (LOGO !== null && LOGO.classList.contains("unactive")) {
@@ -16,4 +23,16 @@ function toggleHeaderButton() {
       LOGO.classList.add("unactive");
     }
   }
+}
+
+function cleanActiveInItem() {
+  SERVICES_LIST.forEach((item) => {
+    if (item.classList.contains("services-item-active")) {
+      item.classList.remove("services-item-active");
+    }
+  });
+}
+
+function addActiveInItem(item) {
+  item.classList.add("services-item-active");
 }
